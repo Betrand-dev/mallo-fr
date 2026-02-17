25b6e8a56028968f8f82bee22c191c56589c731c
Betrand <betrandojong146@gmail.com>
2026-02-17 17:56:55 +0100

    problem with installation

--- mallo/__init__.py
+++ mallo/__init__.py
@@ -7,5 +7,5 @@ from mallo.request import Request
 from mallo.response import Response
 from mallo.cli import cli
 
-__version__ = "0.3.0a3"
+__version__ = "0.3.0a4"
 __all__ = ['Mallo', 'Request', 'Response', 'cli' ]
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 
 setup(
     name="mallo",
-    version="0.3.0a3",
+    version="0.3.0a4",
     author="Akum betrand",
     author_email="betrandojong146@gmail.com",
     description="A lightweight web framework with hot reload",
