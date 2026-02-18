e1716c69a11bd1b600313231fc3e6c56d19ae2aa
Betrand <betrandojong146@gmail.com>
2026-02-18 08:26:45 +0100

    Fix version to valid PEP440 (0.3.0a6)

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 
 setup(
     name="mallo",
-    version="0.3.0a5.1",
+    version="0.3.0a6",
     author="Akum betrand",
     author_email="betrandojong146@gmail.com",
     description="A lightweight web framework with hot reload",
